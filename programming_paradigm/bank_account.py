872e2b8e7d075eda231e9102e36d944ed97e6687
Omar-F-Shoman <omar.f.shoman@gmail.com>
2024-10-06 08:39:32 +0300

    create a simple  bank_account.py script

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+    
+    
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:.2f}")
+
+if __name__ == "__main__":
+     # Example usage
+    account = BankAccount(100)
+    account.deposit(50)
+    account.withdraw(30)
+    account.display_balance()    
